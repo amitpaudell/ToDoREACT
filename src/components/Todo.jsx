f51aaf07fec77fb20da60e224c4c4245d44c6a0d
= <amitpaudel789@gmail.com>
2025-01-18 18:00:46 +0545

    final commit

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,13 +1,18 @@
 import Form from "./Form.jsx";
 import TodoList from "./TodoList.jsx";
 import { useState } from "react";
+import Footer from "./Footer.jsx";
 
 export default function Todo() {
   const [todos, setTodos] = useState([]);
+  const completedTodos = todos.filter((todo) => todo.status).length;
+  const totalTodos = todos.length;
+
   return (
     <>
       <Form todos={todos} setTodos={setTodos}></Form>
       <TodoList todos={todos} setTodos={setTodos}></TodoList>
+      <Footer completedTodos={completedTodos} totalTodos={totalTodos}></Footer>
     </>
   );
 }
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,9 +1,10 @@
 import TodoItem from "./TodoItem.jsx";
 import styles from "./todolist.module.css";
 export default function TodoList({ todos, setTodos }) {
+  const sortedTodos = todos.slice().sort((a, b) => Number(a.status) - Number(b.status));
   return (
     <div className={styles.list}>
-      {todos.map((item) => {
+      {sortedTodos.map((item) => {
         return <TodoItem key={item.name} item={item} todos={todos} setTodos={setTodos} />;
       })}
     </div>
